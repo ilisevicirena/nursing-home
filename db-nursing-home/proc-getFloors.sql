f1f509498ec6bd52111fadc5ec17ba7a6d43673f
Ilišević Irena <iilisevic@it4.ltd>
2023-04-13 15:45:03 +0200

    #2

---
+++ db-nursing-home/proc-getFloors.sql
@@ -0,0 +1,34 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Irena Ilisevic
+-- Create date: 13.4.2023
+-- Description:	Select all floors.
+-- =============================================
+CREATE PROCEDURE [dbo].[getFloors]
+	-- Add the parameters for the stored procedure here
+	
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+SELECT 
+		[Id] = Id,
+		[Name] = Name		
+	FROM [dbo].[Floor]
+END
+GO
