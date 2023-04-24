b10413b3b3de793ff469833466fbc5895e965162
Ilišević Irena <iilisevic@it4.ltd>
2023-04-24 09:21:03 +0200

    #53

--- db-nursing-home/proc-getRoomsForFloor.sql
+++ db-nursing-home/proc-getRoomsForFloor.sql
@@ -1,18 +1,13 @@
--- ================================================
--- Template generated from Template Explorer using:
--- Create Procedure (New Menu).SQL
---
--- Use the Specify Values for Template Parameters 
--- command (Ctrl-Shift-M) to fill in the parameter 
--- values below.
---
--- This block of comments will not be included in
--- the definition of the procedure.
--- ================================================
+USE [ENV01_NURSING_HOME]
+GO
+
+/****** Object:  StoredProcedure [dbo].[getRoomsForFloor]    Script Date: 24.4.2023. 9:20:15 ******/
 SET ANSI_NULLS ON
 GO
+
 SET QUOTED_IDENTIFIER ON
 GO
+
 -- =============================================
 -- Author:		Irena Ilisevic
 -- Create date: 17.4.2023
@@ -33,9 +28,16 @@ BEGIN
 	[Name]=room.Name, 
 	[Capacity]=room.Capacity, 
 	[FloorId]=room.FloorId, 
-	[FloorName]=floor.Name
+	[FloorName]=floor.Name,
+	[People]=isnull(t2.Number, 0)
 FROM [dbo].[Room] as room
+left join (select  count(prr.RoomId) as Number, prr.RoomId
+	from PersonRoomRelation as prr
+		join Person p on prr.PersonId=p.Id	
+		where prr.Active=1 group by prr.RoomId) as t2 on t2.RoomId=room.Id
 INNER JOIN [dbo].[Floor] as floor 
 ON room.FloorId=floor.Id WHERE room.FloorId = @FloorId;
 END
 GO
+
+
