a09f9e2610b0401b7dd788e3952558eb60f0fb9e
Ilišević Irena <iilisevic@it4.ltd>
2023-04-28 09:23:59 +0200

    Update profile.component.ts

--- ngx-nursing-home/src/app/pages/profile/profile.component.ts
+++ ngx-nursing-home/src/app/pages/profile/profile.component.ts
@@ -223,23 +223,19 @@ export class ProfileComponent implements OnInit, OnDestroy {
     var mm = date.getMonth() + 1;
     var dd = date.getDate();
     var years, months, days;
-    // months
     months = month - mm;
     if (day < dd) {
       months = months - 1;
     }
-    // years
     years = year - yy;
     if (month * 100 + day < mm * 100 + dd) {
       years = years - 1;
       months = months + 12;
     }
-    // days
+
     days = Math.floor((today.getTime() - (new Date(yy + years, mm + months - 1, dd)).getTime()) / (24 * 60 * 60 * 1000));
-    //
+
     return { years: years, months: months, days: days };
-
-
   }
 
 }
