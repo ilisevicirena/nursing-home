bbfac1815f358791a21fceaaba96df7e34d2e643
Ilišević Irena <iilisevic@it4.ltd>
2023-05-03 08:54:54 +0200

    Update services-management.service.ts

--- ngx-nursing-home/src/app/services/rest/services-management.service.ts
+++ ngx-nursing-home/src/app/services/rest/services-management.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { BaseRestApiService } from '../base-rest-api.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ServicesManagementService {
+export class ServicesManagementService extends BaseRestApiService {
 
-  constructor() { }
+  constructor(http: HttpClient) {
+    super(http, 'api/services-management');
+  }
+
 }
