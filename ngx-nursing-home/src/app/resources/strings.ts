70509cb6e5046020c96d474f1d2afb230a9acb35
Ilišević Irena <iilisevic@it4.ltd>
2023-06-11 21:53:27 +0200

    Notes in new person form

--- ngx-nursing-home/src/app/resources/strings.ts
+++ ngx-nursing-home/src/app/resources/strings.ts
@@ -170,6 +170,7 @@ const strings: IKeyValue[] = [
     { key: "deleteData", value: "Brisanje podataka" },
     { key: "documentsPersonData", value: "Dodavanje dokumenata" },
     { key: "select", value: "Odaberi" },
+    { key: "notesPersonData", value: "Bilješke za novu osobu" },
     { key: "documentsPersonStepText", value: "Ovdje možeš dodati neke dokumente vezane uz novu osobu (kopije osobnih dokumenata, upute za davanje lijekova, liječničke nalaze i slično). Dodane dokumente možeš kasnije pregledati i uređivati unutar profila osobe. Dokumente ne moraš dodati odmah..." },
 ];
 
