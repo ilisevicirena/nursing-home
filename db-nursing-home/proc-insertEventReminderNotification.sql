d496839a955fa3588a6fe606668fdb25417622da
Ilišević Irena <iilisevic@it4.ltd>
2023-06-23 08:37:18 +0200

    Update proc-insertEventReminderNotification.sql

--- db-nursing-home/proc-insertEventReminderNotification.sql
+++ db-nursing-home/proc-insertEventReminderNotification.sql
@@ -1,18 +1,13 @@
--- ================================================
--- Template generated from Template Explorer using:
--- Create Procedure (New Menu).SQL
---
--- Use the Specify Values for Template Parameters 
--- command (Ctrl-Shift-M) to fill in the parameter 
--- values below.
---
--- This block of comments will not be included in
--- the definition of the procedure.
--- ================================================
+USE [ENV01_NURSING_HOME]
+GO
+
+/****** Object:  StoredProcedure [dbo].[insertEventReminderNotification]    Script Date: 23.6.2023. 8:36:46 ******/
 SET ANSI_NULLS ON
 GO
+
 SET QUOTED_IDENTIFIER ON
 GO
+
 -- =============================================
 -- Author:		Irena Ilisevic
 -- Create date: 22.06.2023.
@@ -21,36 +16,39 @@ GO
 CREATE PROCEDURE [dbo].[insertEventReminderNotification]
 AS
 BEGIN
-    DECLARE @Today DATE = CONVERT(DATE, GETDATE());
+     DECLARE @Today DATE = CONVERT(DATE, GETDATE());
 
-    INSERT INTO dbo.[Notification] (NotificationTypeId, CreationDate, ReadDate, [Read], [Text], LinkId, GoToLink)
-    SELECT 
-        nt.id AS NotificationTypeId,
-        @Today AS CreationDate,
-        NULL AS ReadDate,
-        0 AS [Read],
-        CONCAT('Dogaðaj: ', ce.title, ' je za ', nt.DaysReminder, ' dana!') AS [Text],
-        pl.Id AS LinkId,
-        pl.Link AS GoToLink
-    FROM 
-        dbo.CalendarEvent ce
-    INNER JOIN 
-        dbo.NotificationType nt ON nt.Code = 'reminders'
-    INNER JOIN
-        dbo.PageLink pl ON pl.Code = 'calendar'
-    WHERE 
-        ce.Reminder = 1
-        AND (
-            -- Non-recurring events
-            (ce.Recurring = 0 AND DATEDIFF(DAY, ce.[Start], @Today) >= nt.DaysReminder)
-            OR
-            -- Recurring events (ignore year)
-            (
-                ce.Recurring = 1 
-                AND DATEDIFF(DAY, DATEADD(YEAR, YEAR(ce.[Start]) - YEAR(@Today), ce.[Start]), @Today) >= nt.DaysReminder
-            )
+-- Insert notifications for events before the specified number of days
+-- Insert notifications for events before the specified number of days
+INSERT INTO dbo.[Notification] (NotificationTypeId, CreationDate, ReadDate, [Read], [Text], LinkId, GoToLink)
+SELECT 
+    nt.id AS NotificationTypeId,
+    @Today AS CreationDate,
+    NULL AS ReadDate,
+    0 AS [Read],
+    CONCAT('DogaÄ‘aj: ', ce.title, ' je za ', nt.DaysReminder, ' dana!') AS [Text],
+    pl.Id AS LinkId,
+    pl.Link AS GoToLink
+FROM 
+    dbo.CalendarEvent ce
+INNER JOIN 
+    dbo.NotificationType nt ON nt.Code = 'reminders'
+INNER JOIN
+    dbo.PageLink pl ON pl.Code = 'calendar'
+WHERE 
+    ce.Reminder = 1
+    AND (
+        -- Non-recurring events
+        (ce.Recurring = 0 AND CAST(ce.[Start] AS DATE) >= DATEADD(DAY, nt.DaysReminder, @Today))
+        OR
+        -- Recurring events (ignore year)
+        (
+            ce.Recurring = 1 
+            AND DATEPART(DAYOFYEAR, ce.[Start]) >= DATEPART(DAYOFYEAR, DATEADD(DAY, nt.DaysReminder, @Today))
         )
-        AND nt.[Enabled] = 1;
+    )
+    AND nt.[Enabled] = 1;
+
 
     -- Insert notifications for events that start on the current day and reminders are enabled
     INSERT INTO dbo.[Notification] (NotificationTypeId, CreationDate, ReadDate, [Read], [Text], LinkId, GoToLink)
@@ -59,7 +57,7 @@ BEGIN
         @Today AS CreationDate,
         NULL AS ReadDate,
         0 AS [Read],
-        CONCAT('Dogaðaj: ', ce.title, ' je danas!') AS [Text],
+        CONCAT('DogaÄ‘aj: ', ce.title, ' je danas!') AS [Text],
         pl.Id AS LinkId,
         pl.Link AS GoToLink
     FROM 
@@ -71,7 +69,7 @@ BEGIN
     WHERE 
         ce.Reminder = 1
         AND (
-            ce.[Start] = @Today
+            CAST(ce.[Start] AS DATE) = @Today
             OR
             (
                 ce.Recurring = 1
@@ -80,4 +78,6 @@ BEGIN
         )
         AND nt.[Enabled] = 1;
 END;
+
+GO
 
